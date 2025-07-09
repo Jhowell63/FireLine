0a749874acf0a62255a27ee9747a610c297e039d
James Howell <howelljames621@gmail.com>
2025-07-08 21:16:18 -0500

    Updated main.py

--- FireLine/main.py
+++ FireLine/main.py
@@ -1,2 +1,27 @@
-print('Hello, This will soon be an exciting new way to schedule employees!')
-print('This is a work in progress, so please be patient as we develop this project.')
+from tkinter import *
+from logIn import LoginScreen
+
+
+def launch_app():
+    # Initialize window
+    window = Tk()
+    window.title("FireLine")
+    window.geometry("1200x800")
+
+    # Convert image to photo image
+    logo_path = 'photos/FireLine.png'
+    icon = PhotoImage(file=logo_path)
+    window.iconphoto(True, icon)
+
+    # Change background color
+    window.configure(bg='#E6E6E6')
+
+    # Load in logIn
+    login = LoginScreen(window)
+    login.pack(fill="both", expand=True)
+
+    # Set up window
+    window.mainloop()
+
+if __name__ == "__main__":
+    launch_app()
